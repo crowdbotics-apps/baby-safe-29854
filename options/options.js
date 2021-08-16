e0c17ef3ce82c4ffa9dc5b2a97608c49676225f7
Crowdbotics <team@crowdbotics.com>
2021-08-16 18:07:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "baby_safe_29854Identifier",
+  url: "https://baby_safe_29854Identifier.botics.co",
+  api: "https://baby_safe_29854Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
